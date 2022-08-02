32a0dc68d9d10b773f10b143a498a7ff3098ea0b
Berat Altındal <berat_altindal.97@hotmail.com>
2022-08-02 21:43:38 +0300

    add

---
+++ GirilenSayiyaKadarOlanCiftSayilariBulanProgram.java
@@ -0,0 +1,29 @@
+/**
+ * GirilenSayiyaKadarOlanCiftSayilariBulanProgram
+ */
+import java.util.Scanner;
+
+public class GirilenSayiyaKadarOlanCiftSayilariBulanProgram {
+
+    public static void main(String[] args) {
+        int k,a = 0,b = 0, c = 1;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("0'dan girilen sayıya kadar olan sayılardan\n3 ve 4'e tam bölünen sayıların ortalamasını bulmak istediginiz \nSayı giriniz : ");
+        k = inp.nextInt();
+
+        for (int i = 1 ; i <= k ; i++){
+            if ((i % 3 == 0) && (i % 4 == 0)){
+                a++;
+                b = i + b;
+                
+                
+            }
+
+        }
+        c = b / a;
+        System.out.println("Sayıların ortalaması : "+ c);
+
+
+    }
+}
